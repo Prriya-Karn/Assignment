143f6e80d456687eff00b509e1106a8e0c783d94
Priya Karn <priya.karn.rn51@gmail.com>
2025-01-07 14:34:02 +0530

    more

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+import Tiptap from "./Tiptap"
+
+const App = () => {
+  return (
+    <Fragment>
+      <Tiptap />
+    </Fragment>
+  )
+}
+
+export default App;
---
+++ src/Tiptap.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+
+const Tiptap = ()=>{
+    return(
+        <Fragment>
+        <h1>hi priya karn</h1>
+        </Fragment>
+    )
+}
+
+export default Tiptap;
